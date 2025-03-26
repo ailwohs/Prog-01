8f9cbf04dcf2d7f758b61d6d6a33f9f5981670c4
ailwohs <juliaavancisoares@gmail.com>
2025-03-26 18:48:51 -0300

    alteração

--- aula1.py
+++ aula1.py
@@ -1,13 +1,17 @@
-Exercícios de fixação:
+#Exercícios de fixação
 
-1 - Os pares de instruções abaixo produzem o mesmo resultado?
+'''1 - Os pares de instruções abaixo produzem o mesmo resultado?
 A) (4/2)+(2/4) e A 4/2+2/4
 B) 4/(2+2)/4 e B 4/2+2/4
-C) (4+2)*2-4 e C 4+2*2-4
+C) (4+2)*2-4 e C 4+2*2-4'''
 
-2 -Reescreva as instruções abaixo com o mínimo de parênteses possível, mas sem alterar o resultado:
+
+
+''' 2 -Reescreva as instruções abaixo com o mínimo de parênteses possível, mas sem alterar o resultado:
 A) 6*(3+2) F (6/3)+(8/2)
 B) 2+(6*(3+2)) G ((3+(8/2))*4)+(3*2)
 C) 2+(3*6)/(2+4) H (6*(3*3)+6)-10
 D) 2*(8/(3+1)) I (((10*8)+3)*9)
-E) 3+(16-2)/(2*(9-2)) J ((-12)*(-4))+(3*(-4))
+E) 3+(16-2)/(2*(9-2)) J ((-12)*(-4))+(3*(-4)) '''
+
+print("6*(3+2) + (6/3)+(8/2)")

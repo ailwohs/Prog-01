cc32c504d8821710732fe8a538ad0fe41ba11f30
ailwohs <juliaavancisoares@gmail.com>
2025-03-26 18:51:46 -0300

    alterações + adicionando primeira aula

--- aula1.py
+++ aula1.py
@@ -14,4 +14,27 @@ C) 2+(3*6)/(2+4) H (6*(3*3)+6)-10
 D) 2*(8/(3+1)) I (((10*8)+3)*9)
 E) 3+(16-2)/(2*(9-2)) J ((-12)*(-4))+(3*(-4)) '''
 
-print("6*(3+2) + (6/3)+(8/2)")
+#print ("6*3+26/3+8/2")
+
+
+
+'''3) Um usuário colocou 7 itens no seu carrinho de supermercado. Faça um algoritmo que o usuário irá
+digitar o valor de cada um deste itens. Ao final, o programa deverá exibir na tela:
+a) O item de maior valor
+b) O item de menor valor
+c) O somatório dos valores dos itens.'''
+
+'''num=int(input("Digite o valor dos intens adicionados ao carrinho \t>"))
+maior_valor=num
+menor_valor=num 
+soma=0 
+for i in range(0,7):
+    num = int(input("Digite o restante de itens: \t>"))
+    if (num > maior_valor):
+        maior_valor=num
+    if (num < menor_valor):
+        menor_valor=num
+    soma=soma+num
+print("O item de maior valor é:", maior_valor)
+print("O item de menor valor é:", menor_valor)
+print("A soma de todos os itens é:", soma)'''

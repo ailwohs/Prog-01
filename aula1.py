6680748c4b917dd3a97668300296116a21ee158f
ailwohs <juliaavancisoares@gmail.com>
2025-03-26 21:18:48 -0300

    alterações adicionando atividades da primeira aula

--- aula1.py
+++ aula1.py
@@ -1,9 +1,23 @@
 #Exercícios de fixação
 
 '''1 - Os pares de instruções abaixo produzem o mesmo resultado?
-A) (4/2)+(2/4) e A 4/2+2/4
-B) 4/(2+2)/4 e B 4/2+2/4
-C) (4+2)*2-4 e C 4+2*2-4'''
+A) (4/2)+(2/4) e A 4/2+2/4 V
+B) 4/(2+2)/4 e B 4/2+2/4 F
+C) (4+2)*2-4 e C 4+2*2-4 F
+'''
+
+'''print((4/2)+(2/4))
+print(4/2+2/4)
+print('O resultado é igual')
+
+
+print(4/(2+2)/4 , 4/2+2/4)
+print('O resultado não é igual')
+
+print(4/(2+2)/4 == 4/2+2/4)
+print((4+2)*2-4 == 4+2*2-4)
+
+# == COMPARAÇÃO'''
 
 
 
@@ -14,27 +28,34 @@ C) 2+(3*6)/(2+4) H (6*(3*3)+6)-10
 D) 2*(8/(3+1)) I (((10*8)+3)*9)
 E) 3+(16-2)/(2*(9-2)) J ((-12)*(-4))+(3*(-4)) '''
 
-#print ("6*3+26/3+8/2")
+#A
+print(6*(3+2))
+
+#B
+print(2+6*(3+2))
+
+#C
+print(2+3*6/(2+4))
+
+#D
+print( 2*(8/(3+1)))
+
+#E
+print(3+(16-2)/(2*(9-2)))
+
+#F
+print(6/3+8/2)
+
+#G
+print((3+(8/2))*4+3*2)
+
+#H
+print(6*3*3+6-10)
+
+#I
+print((10*8+3)*9)
+
+#J
+print(-12*-4+3*-4)
 
 
-
-'''3) Um usuário colocou 7 itens no seu carrinho de supermercado. Faça um algoritmo que o usuário irá
-digitar o valor de cada um deste itens. Ao final, o programa deverá exibir na tela:
-a) O item de maior valor
-b) O item de menor valor
-c) O somatório dos valores dos itens.'''
-
-'''num=int(input("Digite o valor dos intens adicionados ao carrinho \t>"))
-maior_valor=num
-menor_valor=num 
-soma=0 
-for i in range(0,7):
-    num = int(input("Digite o restante de itens: \t>"))
-    if (num > maior_valor):
-        maior_valor=num
-    if (num < menor_valor):
-        menor_valor=num
-    soma=soma+num
-print("O item de maior valor é:", maior_valor)
-print("O item de menor valor é:", menor_valor)
-print("A soma de todos os itens é:", soma)'''
---
+++ variavel.py
@@ -0,0 +1,18 @@
+'''138
+B=12
+
+print(A,B)
+print(A+B)
+print(A//B)
+print(A/2)'''
+
+
+
+
+#CALCULAR IMC
+peso=61
+altura=1.65
+
+resultado = peso/altura**2
+print(resultado)
+

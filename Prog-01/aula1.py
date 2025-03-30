2d8f79f706cd13ec68417b3f79c9015bec283c65
Ailwohs <juliaavancisoares@gmail.com>
2025-03-30 19:03:45 -0300

    adicionando tarefas para a aula 02- corrigir a 01

---
+++ Prog-01/aula1.py
@@ -0,0 +1,61 @@
+#Exercícios de fixação
+
+'''1 - Os pares de instruções abaixo produzem o mesmo resultado?
+A) (4/2)+(2/4) e A 4/2+2/4 V
+B) 4/(2+2)/4 e B 4/2+2/4 F
+C) (4+2)*2-4 e C 4+2*2-4 F
+'''
+
+'''print((4/2)+(2/4))
+print(4/2+2/4)
+print('O resultado é igual')
+
+
+print(4/(2+2)/4 , 4/2+2/4)
+print('O resultado não é igual')
+
+print(4/(2+2)/4 == 4/2+2/4)
+print((4+2)*2-4 == 4+2*2-4)
+
+# == COMPARAÇÃO'''
+
+
+
+''' 2 -Reescreva as instruções abaixo com o mínimo de parênteses possível, mas sem alterar o resultado:
+A) 6*(3+2) F (6/3)+(8/2)
+B) 2+(6*(3+2)) G ((3+(8/2))*4)+(3*2)
+C) 2+(3*6)/(2+4) H (6*(3*3)+6)-10
+D) 2*(8/(3+1)) I (((10*8)+3)*9)
+E) 3+(16-2)/(2*(9-2)) J ((-12)*(-4))+(3*(-4)) '''
+
+#A
+print(6*(3+2))
+
+#B
+print(2+6*(3+2))
+
+#C
+print(2+3*6/(2+4))
+
+#D
+print( 2*(8/(3+1)))
+
+#E
+print(3+(16-2)/(2*(9-2)))
+
+#F
+print(6/3+8/2)
+
+#G
+print((3+(8/2))*4+3*2)
+
+#H
+print(6*3*3+6-10)
+
+#I
+print((10*8+3)*9)
+
+#J
+print(-12*-4+3*-4)
+
+
---
+++ Prog-01/correçãoAula01.py
@@ -0,0 +1,16 @@
+'''1 - Os pares de instruções abaixo produzem o mesmo resultado?
+A) (4/2)+(2/4) e A 4/2+2/4 V
+B) 4/(2+2)/4 e B 4/2+2/4 F
+C) (4+2)*2-4 e C 4+2*2-4 F
+'''
+
+print((4/2)+(2/4))
+print(4/2+2/4)
+print('O resultado é igual')
+
+
+print(4/(2+2)/4 , 4/2+2/4)
+print('O resultado não é igual')
+
+print((4+2)*2-4 , 4+2*2-4)
+print("O resultado não é igual")

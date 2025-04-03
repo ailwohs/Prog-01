ec6513ce0218f1b7cb08c987847939e1a32bd5fc
ailwohs <juliaavancisoares@gmail.com>
2025-04-02 21:15:15 -0300

    mais ativididades adicionadas

---
+++ desafios.py
@@ -0,0 +1,15 @@
+'''produto = 1250
+pct_desconto = 20
+desconto = produto * pct_desconto / 100
+prod_com_desconto = produto - desconto
+
+print("O valor de desconto é R$", desconto,  "\n"  "E o valor final do produto com desconto é R$", prod_com_desconto)'''
+
+
+
+
+
+Nome = "julia"
+Sobrenome = "leithold"
+
+print("Meu nome é:", Nome, "e meu sobrenome é:", Sobrenome)
